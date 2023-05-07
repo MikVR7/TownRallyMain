14360834adc8e87dc42f56fac0bba6951ff926ea
MikVR7 <atmikes123@gmail.com>
2023-05-07 11:16:19 +0200

    test push to resolve repo problems

--- Assets/TownRally/Scripts/Rallies/RalliesHandler.cs
+++ Assets/TownRally/Scripts/Rallies/RalliesHandler.cs
@@ -1,9 +1,4 @@
-using Firebase.Database;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace TownRally
